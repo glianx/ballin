6024a052cd743d0faa0d2a10b3a129912fe4bf37
Gordon Liang <gordon.liang@duke.edu>
2025-02-08 18:49:42 -0500

    new commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: true, // Listen on all addresses
+		port: 5173,
+		strictPort: false, // Allow fallback to another port if 5173 is taken
+		open: true // Open browser automatically
+	}
 });
